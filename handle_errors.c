4c78a1b62f692f9b24acb3ce657c3a07c44778e7
melandres8 <melanmosta@gmail.com>
2020-04-17 01:27:05 +0000

    Fix

--- handle_errors.c
+++ handle_errors.c
@@ -2,13 +2,14 @@
 
 /**
  * print_err - Handle the command errors
- * @file:
- * @command:
- * @nerror:
+ * @file: File
+ * @command: Command input
+ * @nerror: Number of error
  */
 void print_err(char *file, char *command, int nerror)
 {
 	char errmsg[720];
+
 	sprintf(errmsg, "%s: %d: %s: not found\n", file, nerror, command);
 	write(STDOUT_FILENO, errmsg, _strlen(errmsg));
 }
